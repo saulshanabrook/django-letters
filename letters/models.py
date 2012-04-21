10646c45e419d787e7a9ced7493791846ff38584
saulshanabrook <saul.shanabrook@gmail.com>
2012-04-21 06:41:30 +0000

    changed models to remove multiple users shared

--- letters/models.py
+++ letters/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+
 class Letter(models.Model):
-    title = models.CharField(max_length=30, unique_for_date="date_time_created")
+    title = models.CharField(max_length=60, unique_for_date="date_time_created")
     content = models.TextField()
     author = models.ForeignKey(User, editable=False)
     show_author = models.BooleanField(default=True, verbose_name='Show the author?')
     date_time_created = models.DateTimeField(auto_now_add=True)
-    users_shared_to = models.ManytoManyField(User, default=self.author.last_letter_shared_to(), verbose_name='Addressed to')
     users_shared_with = models.ManytoManyField(User, default=self.author.last_letter_shared_with(), verbose_name='Shared with')
 
     class Meta:
@@ -37,13 +37,6 @@ class Comment(models.model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     
-    def last_letter_shared_to(self):
-        """Return the last users shared to in a letter"""
-        try:
-            return self.letter_set.latest().user_shared_to
-        except DoesNotExist:
-            return None
-    
     def last_letter_shared_with(self):
         """Return the last users with to in a letter"""
         try:
@@ -52,15 +45,14 @@ class UserProfile(models.Model):
             return None
     
     def all_letter_shared_recipients(self):
-        """Return all the users shared to or with in letters"""
-        return self.letter_set.values('users_shared_to', 'user_shared_with')
-        #write function to aggregate these two
+        """Return all the users shared letters with"""
+        return self.letter_set.values_list('user_shared_with', flat=True)
     
     def last_comment_shared_recipients(self, Letter):
         """Return the last users shared to in the last comment by this user in this letter"""
         return Letter.comment_set.filter(author__exact=self).latest()
     
     def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-    post_save.connect(create_user_profile, sender=User)
+        if created:
+            UserProfile.objects.create(user=instance)
+        post_save.connect(create_user_profile, sender=User)
