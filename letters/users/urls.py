4cca6e219bcbd93a08f3f464fd511f75f07b9e89
Saul Shanabrook <s.shanabrook@gmail.com>
2012-05-17 19:51:32 -0400

    Moved current tests into "Users" app folder

--- letters/users/urls.py
+++ letters/users/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import DetailView
 
+from letters.users.models import User
+
 
 urlpatterns = patterns('',
     url(r'^user/(?P<slug>\w+/)$', DetailView.as_view(model=User)),
